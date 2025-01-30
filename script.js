a94a5d474f0661ed2095408dde178fc773371c13
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-30 09:07:10 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function(){
+	const inputValue = Number(document.getElementById("ip").value);
+	const outputDiv = document.getElementById("output");
+
+	new promise ((resolve)=>{
+		setTimeout(()=>{
+			outputDiv.textContent = `Result:${inputValue}`;
+			resolve(inputValue);	
+		},2000);
+	}))
+	.then(num => new promise((resolve)=>{
+		setTimeout(()=>{
+			const multiplied = num*2;
+			outputDiv.textContent=`Result:${multiplied}`;
+			resolve(multiplied);
+		},1000);
+	}))
+    .then(num => new promise ((resolve) =>{
+		setTimeout(()=>{
+			const add = num+10;
+			outputDiv.textContent = `Final Result: ${finalResult}`;
+			
+		},1000);
+	})
+    .catch(error=>{
+		outputDiv.textContent = `ERROR:${error}`;
+	}); 
+});
